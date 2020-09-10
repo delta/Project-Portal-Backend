795dda4241e87ae951c1cf64aeeb3314bf9ba896
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2020-09-10 23:48:31 +0530

    (add): added project routes

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,11 @@ Route::post('auth/register/', 'AuthController@register');
 //dashboard
 
 //project
+Route::get('/projects/all/', 'ProjectController@all');
+Route::get('/projects/{project_id}/', 'ProjectController@show');
+Route::post('/projects/add/', 'ProjectController@add')->middleware('auth:api');
+Route::post('/projects/{project_id}/edit/', 'ProjectController@edit')->middleware('auth:api');
+Route::post('/projects/{project_id}/delete/', 'ProjectController@delete')->middleware('auth:api');
 
 //filters
 
