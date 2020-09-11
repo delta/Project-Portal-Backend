40d068049ec45ad228587b1fe4b6a0479a44ffa3
geforce6t <shashwatmessi43@gmail.com>
2020-09-11 12:34:36 +0530

    -Remove unnecessary imports -Move feedback routes to api.php -Implement multiple criteria for query -Introduce proper Error response and message

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -5,40 +5,40 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\Feedback;
-use App\Models\ProjectUser;
-use SebastianBergmann\CodeCoverage\Report\Xml\Project as XmlProject;
+use Illuminate\Support\Facades\DB;
 
 class FeedbackController extends Controller
 {
     public function index($project_id)
     {
-        $feedbacks_sent = Project::find($project_id)->feedbacks()->where('sender_id', auth()->user()->id)->get();
-        $feedbacks_recieved = Project::find($project_id)->feedbacks()->where('receiver_id', auth()->user()->id)->get();
+        if (Project::find($project_id)) {
+            $feedbacks_sent = Project::find($project_id)->feedbacks()->where('sender_id', auth()->user()->id)->get();
+            $feedbacks_recieved = Project::find($project_id)->feedbacks()->where('receiver_id', auth()->user()->id)->get();
         
-        return response(['feedback_sent' => $feedbacks_sent, 'feedback_received' => $feedbacks_recieved]);
+            return response(['feedback_sent' => $feedbacks_sent, 'feedback_received' => $feedbacks_recieved]);
+        } else {
+            return response()->json(['message' => 'Project not Found'], 404);
+        }
     }
 
     public function add($project_id, Request $request)
     {
-        $projects =  ProjectUser::where('project_id', $project_id)->get();
-        foreach ($projects as $project) {
-            
-            if ($project->user_id == auth()->user()->id) {
-                $feedback = new Feedback;
+        if (count(DB::table('project_user')->where([
+            ['project_id', '=' ,$project_id], 
+            ['user_id', '=' ,auth()->user()->id]
+            ])->get()) != 0){
+            $feedback = new Feedback;
 
-                $feedback->project_id = $project_id;
-                $feedback->sender_id = auth()->user()->id;
-                $feedback->receiver_id = $request->receiver_id;
-                $feedback->content = $request->content;
+            $feedback->project_id = $project_id;
+            $feedback->sender_id = auth()->user()->id;
+            $feedback->receiver_id = $request->receiver_id;
+            $feedback->content = $request->content;
                 
-                $feedback->save();
-                return $feedback;                
-                //redirect 
-            }
-        }
-
-        return response(['error_message'=> "you are not a part of this group!!!"]);        
-        //redirect to view 
+            $feedback->save();
+        } else{
+            return response()->json(['message' => 'Only Members of Project can add a Feedback'], 403);        
+            //redirect to view 
+        }            
     }
 
     public function edit(Request $request)
@@ -53,24 +53,22 @@ class FeedbackController extends Controller
             //redirect            
 
         } else {
-            return response(['Reject_Message' => 'access denied' ]);
+            return response()->json(['message' => 'Only Creator of a Feedback can edit a Feedback'], 403);
         }        
     }
 
     public function review($project_id, Request $request)
     {
-        $projects =  ProjectUser::where('project_id', $project_id)->get();
-        foreach ($projects as $project) {
-            
-            if ($project->user_id == auth()->user()->id) {
-                if ($project->role == 'MAINTAINER') {
-                    $review_project = Project::find($project_id);
-                    $review_project->review = $request->review;
-                    return $review_project;
-                }             
-            }
-        }
-
-        return response(['error_message' => 'you cannot review this project!!']);
+        if (count(DB::table('project_user')->where([
+            ['project_id', '=' ,$project_id], 
+            ['user_id', '=' ,auth()->user()->id],
+            ['role', '=' , 'MAINTAINER']
+            ])->get()) != 0) {
+                $review_project = Project::find($project_id);
+                $review_project->review = $request->review;
+                return $review_project;
+        } else {
+            return response()->json(['message' => 'Only Maintainers of Project can Review'], 403);
+        }            
     }
 }
