6479ee4bbe7b2f5493d84bb359cbe5ed1964c9c9
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2020-09-21 17:57:10 +0530

    (fix): more rigorous checking

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
@@ -9,36 +10,49 @@ use Illuminate\Support\Facades\DB;
 
 class FeedbackController extends Controller
 {
-    public function index($project_id)
+
+    /**
+     * Get feedbacks sent or received by user of a specific project
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $projectId
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, $projectId)
     {
         try {
-            Project::findOrFail($project_id);
+            $project = Project::findOrFail($projectId);
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'Project doesn\'t exist!'
             ], 404);
         }
-        if (Project::find($project_id)) {
-            $feedbacks_sent = Project::find($project_id)->feedbacks()->where('sender_id', auth()->user()->id)->get();
-            $feedbacks_recieved = Project::find($project_id)->feedbacks()->where('receiver_id', auth()->user()->id)->get();
 
-            return response([
-                'message' => "Successfully fetched feedbacks",
-                'feedback_sent' => $feedbacks_sent,
-                'feedback_received' => $feedbacks_recieved
-            ]);
+        $userId = $request->user()->id;
 
-        } else {
-            return response()->json([
-                'message' => 'Project not Found'
-            ], 404);
-        }
+        $feedbacksSent = $project->feedbacks()->where('sender_id', $userId)->get();
+        $feedbacksRecieved = $project->feedbacks()->where('receiver_id', $userId)->get();
+
+        return response([
+            'message' => "Successfully fetched feedbacks",
+            'data' => [
+                'feedbacks_sent' => $feedbacksSent,
+                'feedbacks_received' => $feedbacksRecieved
+            ]
+        ]);
     }
 
-    public function add($project_id, Request $request)
+    /**
+     * Add feedback
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $projectId
+     * @return \Illuminate\Http\Response
+     */
+    public function add(Request $request, $projectId)
     {
         try {
-            Project::findOrFail($project_id);
+            $project = Project::findOrFail($projectId);
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'Project doesn\'t exist!'
@@ -49,76 +63,142 @@ class FeedbackController extends Controller
             'content' => 'required|max:1000'
         ]);
 
-        if (count(DB::table('project_user')->where([
-            ['project_id', '=' ,$project_id],
-            ['user_id', '=' ,auth()->user()->id]
-            ])->get()) != 0){
-            $feedback = new Feedback;
+        $senderId = $request->user()->id;
+        $receiverId = $request->receiver_id;
 
-            $feedback->project_id = $project_id;
-            $feedback->sender_id = auth()->user()->id;
-            $feedback->receiver_id = $request->receiver_id;
-            $feedback->content = $request->content;
-
-            $feedback->save();
-        } else{
+        if ($senderId === $receiverId) {
             return response()->json([
-                'message' => 'Only Members of Project can add a Feedback'
+                'message' => 'You can\'t add feedback to yourself'
+            ], 403);
+        }
+        $projectUsers = $project->users()->get();
+        if (!$projectUsers->contains($senderId)) {
+            return response()->json([
+                'message' => 'Only project members can add feedback'
+            ], 403);
+        } else if (!$projectUsers->contains($receiverId)) {
+            return response()->json([
+                'message' => 'Only project members can receive feedback'
             ], 403);
         }
 
-        return response()->json([
-            'message' => 'Feedback added successfully'
-        ], 200);
+        $feedback = new Feedback;
+
+        $feedback->project_id = $projectId;
+        $feedback->sender_id = $senderId;
+        $feedback->receiver_id = $receiverId;
+        $feedback->content = $request->content;
+
+        DB::transaction(function () use ($project, $feedback) {
+            $feedback->save();
+
+            $project->feedbacks()->save($feedback);
+            $project->save();
+        });
+
+        if ($feedback->exists) {
+            return response()->json([
+                'message' => 'Feedback added successfully!'
+            ], 200);
+        } else {
+            return responsee()->json([
+                'message' => 'Feedback could not be added'
+            ], 503);
+        }
     }
 
+    /**
+     * Edit existing feedback
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function edit(Request $request)
     {
         $request->validate([
-            'feedback_id' => 'required|exists:feedbacks,id',
+            'feedback_id' => 'required|integer|exists:feedbacks,id',
             'content' => 'required|max:1000'
         ]);
-        $feedback = Feedback::find($request->feedback_id);
 
-        if ($feedback->sender_id == auth()->user()->id) {
-
-            $feedback->content = $request->content;
-            $feedback->save();
+        try {
+            $feedback = Feedback::find($request->feedback_id);
+        } catch (\Exception $e) {
             return response()->json([
-                'message' => 'Feedback edited successfully'
-            ], 200);
+                'message' => 'Feedback doesn\'t exist'
+            ], 404);
+        }
 
-        } else {
+        if ($feedback->sender_id != $request->user()->id) {
             return response()->json([
                 'message' => 'Only Creator of a Feedback can edit a Feedback'
             ], 403);
         }
+
+        $feedback->content = $request->content;
+
+        \DB::transaction(function () use ($feedback) {
+            $feedback->save();
+        });
+
+        if ($feedback->content == $request->content) {
+            return response()->json([
+                'message' => 'Feedback edited successfully!'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Feedback could not be edited'
+            ], 503);
+        }
     }
 
-    public function review($project_id, Request $request)
+    /**
+     * Add review
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $projectId
+     * @return \Illuminate\Http\Response
+     */
+    public function review(Request $request, $projectId)
     {
         $request->validate([
             'review' => 'required|max:1000'
         ]);
+
         try {
-            Project::findOrFail($project_id);
+            $project = Project::findOrFail($projectId);
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'Project doesn\'t exist!'
             ], 404);
         }
-        if (count(DB::table('project_user')->where([
-            ['project_id', '=' ,$project_id],
-            ['user_id', '=' ,auth()->user()->id],
-            ['role', '=' , 'MAINTAINER']
-            ])->get()) != 0) {
-                $review_project = Project::find($project_id);
-                $review_project->review = $request->review;
-                return $review_project;
+
+        $user = $request->user();
+
+        if (!$project->users()
+            ->where(function ($query) {
+                $query->where('project_user.role', 'AUTHOR')
+                    ->orWhere('project_user.role', 'MAINTAINER');
+            })->get()
+            ->contains($user->id)) {
+            return response()->json([
+                'message' => 'Only authors or maintainers are allowed to add reviews'
+            ], 403);
+        }
+
+        $project->review = $request->review;
+
+        DB::transaction(function () use ($project) {
+            $project->save();
+        });
+
+        if ($project->review == $request->review) {
+            return response()->json([
+                'message' => 'Review added successfully!'
+            ], 200);
         } else {
             return response()->json([
-                'message' => 'Only Maintainers of Project can Review'
-            ], 403);
+                'message' => 'Review could not be added'
+            ], 503);
         }
     }
 }
