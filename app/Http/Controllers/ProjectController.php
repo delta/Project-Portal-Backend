11b356cd3c5cfebcb5649da28d2779bbdb95c067
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2020-10-03 01:28:27 +0530

    (fix): disallow editing of author roles

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -185,7 +185,7 @@ class ProjectController extends Controller
             'type' => 'required|exists:types,id',
             'users' => 'nullable|array',
             'users.*.id' => 'required|exists:users,id',
-            'users.*.role' => 'required|in:AUTHOR,MAINTAINER,DEVELOPER',
+            'users.*.role' => 'required|in:MAINTAINER,DEVELOPER',
             'stacks' => 'required|array|min:1',
             'stacks.*' => 'exists:stacks,id'
         ]);
@@ -209,6 +209,14 @@ class ProjectController extends Controller
             $project->save();
 
             if (isset($data['users'])) {
+                $authors = $project->users()->wherePivot('role', 'AUTHOR')->get();
+                $project->users()->sync([]);
+                foreach ($authors as $author) {
+                    $project->users()->attach([
+                        $author->id =>
+                        ['role' => 'AUTHOR']
+                    ]);
+                }
                 foreach ($data['users'] as $projectUser) {
                     $project->users()->syncWithoutDetaching([
                         $projectUser['id'] =>
@@ -216,6 +224,7 @@ class ProjectController extends Controller
                     ]);
                 }
             }
+            $project->stacks()->sync([]);
             $project->stacks()->syncWithoutDetaching($data['stacks']);
             $project->save();
         });
@@ -285,7 +294,7 @@ class ProjectController extends Controller
     {
         $filter = User::where('id', $user_id)->with(
          'projects.stacks',
-         'projects.type', 
+         'projects.type',
          'projects.status')->get('id');
 
         return response()->json([
@@ -300,7 +309,7 @@ class ProjectController extends Controller
     {
         $filter = Stack::where('id', $stack_id)->with(
          'projects.stacks',
-         'projects.type', 
+         'projects.type',
          'projects.status')->get('id');
 
         return response()->json([
@@ -315,7 +324,7 @@ class ProjectController extends Controller
     {
         $filter = Type::where('id', $type_id)->with(
          'projects.stacks',
-         'projects.type', 
+         'projects.type',
          'projects.status')->get('id');
 
         return response()->json([
