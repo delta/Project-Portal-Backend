1fa37ff87b17f926b58d4e029c809d3436963dda
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-06-23 10:01:54 +0530

    don't migrate before every test (for performance reasons)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,13 @@
 
 namespace Tests;
 
+use Database\Seeders\TestSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use MigrateAndSeedOnce;
+
+    protected bool $seed = true;
+    protected string $seeder = TestSeeder::class;
 }
